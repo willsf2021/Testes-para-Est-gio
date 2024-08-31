e630919aab52c8c01ef994fa702dafc09919c779
willsf2021 <willsf2021@gmail.com>
2024-08-30 21:55:14 -0300

    Ex.03
    3) Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
    
    Ao final do processamento, qual será o valor da variável SOMA?

---
+++ ex03soma.js
@@ -0,0 +1,12 @@
+// Declaração das variáveis fornecedidas
+let indice = 12;
+let soma = 0;
+let k = 1;
+// Loop solicitado
+while (k < indice) {
+  k++;
+  soma = soma + k;
+}
+
+// Resultado:
+console.log(soma);
